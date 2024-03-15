c0e1c52f9503431154ccee56dccafae1131eae68
Gregory Ruan <gregorysouzagr@gmail.com>
2024-03-15 11:32:22 -0300

    construindo a header da aplicação

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'luiz otavio'
     })
 
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
